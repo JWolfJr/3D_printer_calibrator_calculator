88bd743d133df06c62c759c649d9711a0c9bd014
Jack Wolf <mystinker08@gmail.com>
2023-02-27 20:10:19 -0600

    basic working screen, added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple calculator for 3-D printer extruder calibration. 
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRectangleFlatButton
 
+
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Amber"
+        self.theme_cls.primary_hue = '200'
+        self.theme_cls.theme_style = "Dark"
+    
+        screen = Screen()
+        extrude_btn = MDRectangleFlatButton(text="Hello World", pos_hint={'center_x': 0.5, 'center_y': 0.6})
+        flow_btn = MDRectangleFlatButton(text="Hello World 2", pos_hint={'center_x': 0.5, 'center_y': 0.2})
+        screen.add_widget(extrude_btn)
+        screen.add_widget(flow_btn)
+        return screen
+
+    
+
+MainApp().run()
